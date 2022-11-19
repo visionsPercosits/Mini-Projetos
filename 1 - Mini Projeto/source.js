3a1caf22fdbb637f96b4e8606763267259176759
Cross <crossdev2022@gmail.com>
2022-11-19 14:48:31 -0300

    Mini Projeto - Troca de Valores

---
+++ 1 - Mini Projeto/source.js
@@ -0,0 +1,23 @@
+let a = 'Vermelho';
+let b = 'Azul';
+
+// console.log(a);
+// console.log(b);
+
+
+function TrocarValor(trocar){
+    a = trocar;
+};
+
+
+// console.log(a);
+TrocarValor('Azul');
+console.log(a);
+
+
+function TrocarValor2(trocar2){
+    b = trocar2;
+};
+
+TrocarValor2('Vermelho');
+console.log(b);
