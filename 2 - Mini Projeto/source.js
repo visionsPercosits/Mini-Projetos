e19ec9a9ee56fd92128799dd7988cdba85025358
Cross <crossdev2022@gmail.com>
2022-11-21 18:34:24 -0300

    Vendo valor máximo entre dois números

---
+++ 2 - Mini Projeto/source.js
@@ -0,0 +1,16 @@
+let numero1 = 30;
+let numero2 = 50;
+
+
+if (numero1 > numero2){
+    console.log(`o ${numero1} é maior do que o ${numero2}`);
+}
+else if (cor2 > cor1){
+    console.log(`o ${numero2} é maior do que o ${numero1}`);
+}
+else {
+    console.log('Nenhum valor foi encontrado');
+}
+
+
+// Mostrando mais um pouco da minha evolução 🙂
